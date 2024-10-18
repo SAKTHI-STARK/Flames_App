3d32fa3613141849e4074f0fcab9f8ec03d3b8ac
SAKTHIVEL M <sakthi492003@gmail.com>
2024-10-19 05:28:56 +0530

    modified main window

--- front_end.py
+++ front_end.py
@@ -2,7 +2,6 @@ from tkinter import *
 import tkinter as tk
 from tkinter.font import Font
 # from flames import *
-import time
 #function for the loading window of the app
 # window=Tk()
 window=tk.Tk()
@@ -13,12 +12,29 @@ def main_window(Name_app,author_name):
     author_name.place_forget()
     #to resize the loading window to main window
     window.geometry("600x400")
-        
+    def main_window_elements():
+        #display flames calculator in the main window
+        Header_font=Font(size=22,weight='bold')
+        Header=tk.Label(window,text="FLAMES CALCULATOR",bg="blanchedalmond",fg="deeppink4",font=Header_font)
+        Header.grid(row=0,column=3)
+        #labels to get show what to do Name_1 
+        Name_1=tk.Label(window,text="Your Name",bg="blanchedalmond",fg="black")
+        Name_1.grid(row=1,column=0)
+        Name_1_val=tk.Entry()
+        Name_1_val.grid(row=1,column=2)
+        #labels to get show what to do Name_2
+        Name_2=tk.Label(window,text="Crush Name",bg="blanchedalmond",fg="black")
+        Name_2.grid(row=2,column=0)
+        Name_2_val=tk.Entry()
+        Name_2_val.grid(row=2,column=2)
+    
+    main_window_elements()
+    
 def loading():
     window.geometry("400x200")
     window.configure(bg="blanchedalmond")
     #icon for our app title
-    icon=PhotoImage(file="Flames_App\App_images\icons8-heart-50.png")
+    icon=PhotoImage(file="App_images\icons8-heart-50.png")
     window.iconphoto(True,icon)
     window.title("FLAMES CALCULATOR")
     #creating name for our app name
