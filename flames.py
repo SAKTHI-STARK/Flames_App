895647c1d924adbcc278434f43519c3d067b59fa
SAKTHIVEL M <sakthi492003@gmail.com>
2024-10-19 15:01:59 +0530

    pre final

--- flames.py
+++ flames.py
@@ -1,7 +1,3 @@
-#getting names from the user
-#removing sapace characters from the string or name
-name_1=input("Enter your name:").replace(" ","")
-name_2=input("Enter your crush name:").replace(" ","")
 #function to getting the values of the string input
 def fre_calculator(list_str):
     frequency_str={}
@@ -27,36 +23,38 @@ def flames_calculator(sum):
         else:
             arr.pop(val-1)
     return arr
-#convert string to list
-list_str_1=list(name_1)
-list_str_2=list(name_2)
-#getting the frequency of the values in string 1
-frequency_str_1=fre_calculator(list_str_1)
-#getting the frequency of the values in string 2
-frequency_str_2=fre_calculator(list_str_2)
-#getting the common values from the two string
-common_keys=frequency_str_1.keys() & frequency_str_2.keys()
-#sum is the final value to do flames
-sum=0
-for i in common_keys:
-    val_1=abs(frequency_str_1[i]-frequency_str_2[i])
-    sum=sum+val_1 
-#pop the common values from the two dictionaries
-for i in common_keys:
-    frequency_str_1.pop(i)
-    frequency_str_2.pop(i)   
-#adding the remaining frequence to sum value
-for i in frequency_str_1:
-    val=frequency_str_1[i]
-    sum=sum+val
-for i in frequency_str_2:
-    val=frequency_str_2[i]
-    sum=sum+val
-#passing the final value to calculator function
-result=flames_calculator(sum)
-#dictionaries to show sentence by matching the result
-sentence_dic={'F':"You Have an Amazing Friend",'L':"You Find Your Soulmate with Amazing Love",'A':"You just got Affection with Her",'M':"You Find Your Marriage Partner",'E':"Enimes Found Carefull",'S':"Sibiling in Nature"}
-print(sentence_dic[result[0]])
+#function to communicate with front_end
+def back_end(name_1,name_2):
+    #convert string to list
+    list_str_1=list(name_1.replace(" ",""))
+    list_str_2=list(name_2.replace(" ",""))
+    #getting the frequency of the values in string 1
+    frequency_str_1=fre_calculator(list_str_1)
+    #getting the frequency of the values in string 2
+    frequency_str_2=fre_calculator(list_str_2)
+    #getting the common values from the two string
+    common_keys=frequency_str_1.keys() & frequency_str_2.keys()
+    #sum is the final value to do flames
+    sum=0
+    for i in common_keys:
+        val_1=abs(frequency_str_1[i]-frequency_str_2[i])
+        sum=sum+val_1 
+    #pop the common values from the two dictionaries
+    for i in common_keys:
+        frequency_str_1.pop(i)
+        frequency_str_2.pop(i)   
+    #adding the remaining frequence to sum value
+    for i in frequency_str_1:
+        val=frequency_str_1[i]
+        sum=sum+val
+    for i in frequency_str_2:
+        val=frequency_str_2[i]
+        sum=sum+val
+    #passing the final value to calculator function
+    result=flames_calculator(sum)
+    #dictionaries to show sentence by matching the result
+    sentence_dic={'F':"You Have an Amazing Friend",'L':"You Find Your Soulmate with Amazing Love",'A':"You just got Affection with Her",'M':"You Find Your Marriage Partner",'E':"Enimes Found Carefull",'S':"Sibiling in Nature"}
+    return sentence_dic[result[0]]
 
         
 
--- front_end.py
+++ front_end.py
@@ -1,12 +1,10 @@
 from tkinter import *
 import tkinter as tk
 from tkinter.font import Font
-# from flames import *
+from flames import *
 #function for the loading window of the app
-# window=Tk()
 window=tk.Tk()
-
-
+#function for the main window
 def main_window(Name_app,author_name):
     #to dissapear existing text in the window
     Name_app.place_forget()
@@ -23,17 +21,22 @@ def main_window(Name_app,author_name):
         Name_1.place(x=180,y=80)
         Name_1_val=tk.Entry()
         Name_1_val.place(x=260,y=80,width=200)
-       
         #labels to get show what to do Name_2
         Name_2=tk.Label(window,text="Crush Name",bg="blanchedalmond",fg="black")
         Name_2.place(x=180,y=110)
         Name_2_val=tk.Entry()
         Name_2_val.place(x=260,y=110,width=200)
-
-        
-    
+        #function to communicate with backend
+        def connection():
+            value_1=Name_1_val.get()
+            value_2=Name_2_val.get()
+            result=back_end(value_1,value_2)
+            print(result)
+        #creating button to call function
+        Button_calculate=Button(window,text="FIND",fg="RED",command=connection)
+        Button_calculate.place(x=300,y=160)
     main_window_elements()
-    
+#function for loading window
 def loading():
     window.geometry("400x200")
     window.configure(bg="blanchedalmond")
@@ -50,10 +53,8 @@ def loading():
     author_name=tk.Label(window,text="by SAKTHI-STARK",bg="blanchedalmond",fg="deeppink4",font=Author_name_font)
     author_name.place(x=153,y=110)
     #transforming window to new size 
-    window.after(1000,lambda:main_window(App_name,author_name))
+    window.after(700,lambda:main_window(App_name,author_name))
     window.mainloop()
-    
-        
 loading()
 
 
